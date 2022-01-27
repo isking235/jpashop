e86e9e3a9a36701d3338765c3e9f43579d9ad303
sangho235 <sangho235@it.gsshop.com>
2022-01-27 22:24:54 +0900

    주문도매인개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,18 +1,32 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 
 public class OrderService {
     
-    private final  OrderRepository orderRepository;
-    private final  MemberRepository memberRepository;
-    private final  ItemRepository itemRepository;
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
     
     
     /**
     *주문
     */
-    @Trasactional
+    @Transactional
     public Long order(Long memberId, Long itemId, int count) {
         
         //엔티티 조회
@@ -30,7 +44,7 @@ public class OrderService {
         Order order = Order.createOrder(member, delivery, orderItem);
         
         //주문 저장
-        orderkRepository.save(order);
+        orderRepository.save(order);
         
         return order.getId();
         
