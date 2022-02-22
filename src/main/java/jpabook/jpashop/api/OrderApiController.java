b18115b27c43095f19e219bb91aa6a7f3aece617
sangho235 <sangho235@it.gsshop.com>
2022-02-22 22:43:16 +0900

    주문 조회 v5

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -87,6 +87,11 @@ public class OrderApiController {
 
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
 
     @Getter
     static class OrderDto {
